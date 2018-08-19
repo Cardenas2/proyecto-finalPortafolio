ff2fb2a827ff53e523c908d06b985aacba5167d2
Cardenas2 <vaniacardenas-21@hotmail.com>
2018-08-19 13:46:44 -0300

    colocar en archivo js masonry funcion  images loader

--- assets/js/masonry.js
+++ assets/js/masonry.js
@@ -8,3 +8,8 @@ var $grid = $('.grid').masonry({
 $grid.on( 'removeComplete', function( event, removedItems ) {
   console.log( 'Masonry remove complete with ' + removedItems.length + ' items' );
 });
+
+// layout Masonry after each image loads
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry('layout');
+});
