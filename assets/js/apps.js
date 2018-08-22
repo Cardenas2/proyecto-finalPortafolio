38f20c8e54d78c94b116eac3970e0db3486d006c
Cardenas2 <vaniacardenas-21@hotmail.com>
2018-08-21 21:47:44 -0300

    crear boton para subir en seccion portafolio

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -7,4 +7,22 @@ $(document).ready(function(){
 	$grid.imagesLoaded().progress( function() {
 	  $grid.masonry('layout');
 	});
+
+
+
+
+
+	$(window).scroll(function(){
+ // Aquí va el código que se ejecuta mientras haces el scroll y debe estar fuera del $(window).ready()
+ if ($(window).scrollTop() > altura){
+
+    $('.menuPortafolio1').addClass('fixed-top');
+   }
+ // Esta variable almacenaría la distancia con el top, para poder comparar y así ejecutar algún script en cierto minuto
+ else{
+    $('.enuPortafolio1').removeClass('fixed-top');
+   }
 });
+
+});
+
