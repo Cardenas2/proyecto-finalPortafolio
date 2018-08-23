17575badadc9bd1faa282c035a7245932bb70ec1
Cardenas2 <vaniacardenas-21@hotmail.com>
2018-08-23 20:33:08 -0300

    trabajar documento apps.js

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -1,25 +1,61 @@
 $(document).ready(function(){
-	// init Masonry
+  var total = $('.textoEscrito__magico span').length;
+  var current = 0;
+  
+  
+  function show(){
+    console.log('show', current);
+    $('.current').removeClass('current');
+    $($('.textoEscrito__magico span').get(current)).addClass('current');
+    current++;
+    if(current >= total){
+      current = 0;
+      console.log('reset')
+    }
+    setTimeout(function(){
+      show();
+    }, 3000);
+  }
+  
+  show();
+  
+  
+
+  // init Masonry
 	var $grid = $('.grid').masonry({
 	  // options...
 	});
 	// layout Masonry after each image loads
 	$grid.imagesLoaded().progress( function() {
 		$grid.masonry('layout');
+	}); 
+
+	//funcion boton para up
+	$('.btnInicio').css('opacity', 0);
+		
+	$(window).scroll(function(){
+		 // Aquí va el código que se ejecuta mientras haces el scroll y debe estar fuera del $(window).ready()
+		 if ($(window).scrollTop() <= $(window).height()){
+ 			$('.btnInicio').css('opacity', 0);
+ 		}else{
+ 	 		// Esta variable almacenaría la distancia con el top, para poder comparar y así ejecutar algún script en cierto minuto
+ 			$('.btnInicio').css('opacity', 1);
+ 			//$('.btnInicio').removeClass('fixed-top');
+ 		}
 	});
 
+	$('.btnInicio').click(function(evt){
+		$('html, body').animate({
+			scrollTop: 0
+		})
 
-	$(window).scroll(function(){
- // Aquí va el código que se ejecuta mientras haces el scroll y debe estar fuera del $(window).ready()
- if ($(window).scrollTop() > 1500){
-
- 	$('.btnInicio').addClass('fixed-top');
- }
- // Esta variable almacenaría la distancia con el top, para poder comparar y así ejecutar algún script en cierto minuto
- else{
- 	$('.btnInicio').removeClass('fixed-top');
- }
-});
+		evt.preventDefault();
+		evt.stopPropagation();
+	})
 
 });
+
+
+
+
 
